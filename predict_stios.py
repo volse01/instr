b99168df3e4a2aba2f129d7d5e99bbaf8951e70c
Johannes Volz <volzjohannes@t-online.de>
2024-01-26 13:46:03 +0100

    fitting to use-case

--- predict_stios.py
+++ predict_stios.py
@@ -42,7 +42,7 @@ def main():
     else:
         print('Modifying subpixel correlation layer to fit synthetic data intrinsics')
         time.sleep(1)
-        net.adapt_to_new_intrinsics(f_new=1082.28 / (1280 / 720), b_new=0.12)
+        net.adapt_to_new_intrinsics(f_new=585.121 / (1280 / 720), b_new=0.12)
 
     net = net.cuda().eval()
 
@@ -63,45 +63,83 @@ def main():
             # reset confusion matrix per folder
             ious = []
             f1s = []
-            num_dets_diff = []
-            det_75_ratio = []
+            rec = []
+            pre = []
             # load images
-            for (left, right, depth, gt) in tqdm(paths[sensor][folder]):
-                mat = ConfusionMatrix(threshold=0.5)
+            if args.zed or args.rc_visard:
+                for (left, right, depth, gt) in tqdm(paths[sensor][folder]):
+                    mat = ConfusionMatrix(threshold=0.5)
 
-                left = Image.open(left)
-                right = Image.open(right)
-                left_t = process_im(left)
-                right_t = process_im(right)
+                    left = Image.open(left)
+                    right = Image.open(right)
+                    left_t = process_im(left)
+                    right_t = process_im(right)
 
-                gt = np.array(Image.open(gt))
-                gt = torch.from_numpy(gt).unsqueeze(0)
+                    gt = np.array(Image.open(gt))
+                    gt = torch.from_numpy(gt).unsqueeze(0)
 
-                with torch.no_grad():
-                    preds = net({'color_0': left_t, 'color_1': right_t})
-                pred = preds['predictions_0'][0].unsqueeze(0)
+                    with torch.no_grad():
+                        preds = net({'color_0': left_t, 'color_1': right_t})
+                    pred = preds['predictions_0'][0].unsqueeze(0)
 
-                mat(pred, targets=gt)
-                ious.append(mat.get_iou().item())
-                f1s.append(mat.get_f1().item())
+                    mat(pred, targets=gt)
+                    ious.append(mat.get_iou().item())
+                    f1s.append(mat.get_f1().item())
+                    rec.append(mat.get_rec().item())
+                    pre.append(mat.get_pre().item())
+            else:
+                for (left, right, depth, segmaps) in tqdm(paths[sensor][folder]):
+                    mat = ConfusionMatrix(threshold=0.5)
+
+                    left = Image.open(left)
+                    right = Image.open(right)
+                    left_t = process_im(left)
+                    right_t = process_im(right)
+
+                    segmaps = np.array(Image.open(segmaps))
+                    segmaps = torch.from_numpy(segmaps).unsqueeze(0)
+
+                    with torch.no_grad():
+                        preds = net({'color_0': left_t, 'color_1': right_t})
+                    pred = preds['predictions_0'][0].unsqueeze(0)
+
+                    mat(pred, targets=segmaps)
+                    ious.append(mat.get_iou().item())
+                    f1s.append(mat.get_f1().item())
+                    rec.append(mat.get_rec().item())
+                    pre.append(mat.get_pre().item())
 
             results[folder] = {
                 'ious': ious,
                 'f1s': f1s,
-                'num_dets_diff': num_dets_diff,
-                'det_75': det_75_ratio
+                'rec': rec,
+                'pre': pre
             }
 
     mean = []
+    print('mIoU:')
     for f, folder in enumerate(results.keys()):
         print(f"{sensor}: {folder}: {np.mean(results[folder]['ious'])*100:.4f}")
         mean.extend(results[folder]['ious'])
-    print(f"---\nmean: {np.mean(mean)*100:.4f}\n")
+    print(f"---\nmean mIoU: {np.mean(mean)*100:.4f}\n")
     mean = []
+    print('fi:')
     for f, folder in enumerate(results.keys()):
         print(f"{sensor}: {folder}: {np.mean(results[folder]['f1s'])*100:.4f}")
         mean.extend(results[folder]['f1s'])
-    print(f"---\nmean: {np.mean(mean)*100:.4f}\n")
+    print(f"---\nmean f1: {np.mean(mean)*100:.4f}\n")
+    mean = []
+    print('Recall:')
+    for f, folder in enumerate(results.keys()):
+        print(f"{sensor}: {folder}: {np.mean(results[folder]['rec']) * 100:.4f}")
+        mean.extend(results[folder]['rec'])
+    print(f"---\nmean Recall: {np.mean(mean) * 100:.4f}\n")
+    mean = []
+    print('Precision:')
+    for f, folder in enumerate(results.keys()):
+        print(f"{sensor}: {folder}: {np.mean(results[folder]['pre']) * 100:.4f}")
+        mean.extend(results[folder]['pre'])
+    print(f"---\nmean precision: {np.mean(mean) * 100:.4f}\n")
 
 
 if __name__ == '__main__':
